d07f41c45e8dcd2eec1fe66d9d4a05fc4e8687bc
Gizachew Bayness <eleccrazy24@gmail.com>
2022-06-15 05:42:43 -0700

    Fixing and Adding features

--- main.c
+++ main.c
@@ -1,141 +1,134 @@
-#include "main.h"
+#include "shell.h"
+
+void sig_handler(int sig);
+int execute(char **args, char **front);
 
 /**
- * handle_signal - Handles signal and prints a new prompt
- *
- * @sig: The signal
- *
+ * sig_handler - Prints a new prompt upon a signal.
+ * @sig: The signal.
  */
-
-void handle_signal(__attribute__((unused)) int sig)
+void sig_handler(int sig)
 {
-	signal(SIGINT, handle_signal);
-	prints("\n$ ");
+	char *new_prompt = "\n$ ";
+
+	(void)sig;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 3);
 }
 
 /**
- * split_buffer - splits a string into tokens and stores them
- * in a character array.
+ * execute - Executes a command in a child process.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
  *
- * @buffer: The string to be splitted.
- *
- * Return: A pointer to the array of characters.
- *
+ * Return: If an error occurs - a corresponding error code.
+ *         O/w - The exit value of the last executed command.
  */
-
-char **split_buffer(char *buffer)
+int execute(char **args, char **front)
 {
-	int index = 0;
-	char *token;
-	char **array;
+	pid_t child_pid;
+	int status, flag = 0, ret = 0;
+	char *command = args[0];
 
-	if (buffer == NULL)
-		return (NULL);
-
-	array = malloc(sizeof(char) * 1024);
-	if (array == NULL)
-		return (NULL);
-
-	token = my_strtok(buffer, "\n ");
-
-	for (; token; index++)
+	if (command[0] != '/' && command[0] != '.')
 	{
-		array[index] = token;
-		token = my_strtok(NULL, "\n ");
+		flag = 1;
+		command = get_location(command);
 	}
 
-	array[index] = NULL;
-
-	return (array);
-}
-
-/**
- * excute_command - Executes simple shell command
- *
- * @cmd: The command
- * @b: The user input
- * @c: Counter
- * @filename: The name of the program
- *
- * Return: 0 on success, -1 on failure
- *
- */
-
-int excute_command(char **cmd, char *b, int c, char *filename)
-{
-	pid_t pid;
-
-	if (*cmd == NULL)
-		return (-1);
-
-	pid = fork();
-	if (pid == -1)
+	if (!command || (access(command, F_OK) == -1))
 	{
-		perror("Error");
-		return (-1);
+		if (errno == EACCES)
+			ret = (create_error(args, 126));
+		else
+			ret = (create_error(args, 127));
 	}
-
-	if (pid == 0)
-	{
-		if (_strncmp(*cmd, "./", 2) != 0 && _strncmp(*cmd, "/", 1) != 0)
-			command_path(cmd);
-		if (execve(*cmd, cmd, environ) == -1)
-		{
-			print_error(cmd[0], c, filename);
-			free(b);
-			free(cmd);
-			exit(EXIT_FAILURE);
-		}
-		return (EXIT_SUCCESS);
-	}
-
 	else
-		wait(NULL);
-
-	return (0);
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			if (flag)
+				free(command);
+			perror("Error child:");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			execve(command, args, environ);
+			if (errno == EACCES)
+				ret = (create_error(args, 126));
+			free_env();
+			free_args(args, front);
+			free_alias_list(aliases);
+			_exit(ret);
+		}
+		else
+		{
+			wait(&status);
+			ret = WEXITSTATUS(status);
+		}
+	}
+	if (flag)
+		free(command);
+	return (ret);
 }
 
 /**
- * main - Implements simple shell in both interactive and
- * non-interactive modes.
+ * main - Runs a simple UNIX command interpreter.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * @argc: The number of arguments that it accepts.
- * @argv: The array of pointers to the arguments supplied
- *
- * Return: Exit value by the status.
- *
+ * Return: The return value of the last executed command.
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *buffer;
-	char **cmd;
-	int c = 0;
+	int ret = 0, retn;
+	int *exe_ret = &retn;
+	char *prompt = "$ ", *new_line = "\n";
 
-	(void)argc;
+	name = argv[0];
+	hist = 1;
+	aliases = NULL;
+	signal(SIGINT, sig_handler);
 
-	signal(SIGINT, handle_signal);
+	*exe_ret = 0;
+	environ = _copyenv();
+	if (!environ)
+		exit(-100);
+
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+			ret = handle_args(exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
 
 	while (1)
 	{
-		c++;
-		if (isatty(STDIN_FILENO))
-			prints("$ ");
-
-		buffer = my_getline();
-
-		if (buffer[0] == '\0')
-			continue;
-
-		cmd = split_buffer(buffer);
-
-		if (_strcmp(cmd[0], "exit") == 0)
-			shell_exit(cmd, buffer, argv[0], c);
-
-		excute_command(cmd, buffer, c, argv[0]);
-
-		custome_free(cmd, buffer);
+		write(STDOUT_FILENO, prompt, 2);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
+		{
+			if (ret == END_OF_FILE)
+				write(STDOUT_FILENO, new_line, 1);
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
+		}
 	}
 
-	return (0);
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
 }
